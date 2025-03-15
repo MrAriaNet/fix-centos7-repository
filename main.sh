b5ac26f96b4ef95649719e43f9f8a638ae070aea
Aria <47712536+MrAriaNet@users.noreply.github.com>
2025-03-15 17:19:15 +0330

    Update main.sh
    update script and added epel.repo

--- main.sh
+++ main.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-RANDOM_NUMBER=$(($RANDOM % 900 + 100))
 
-echo "Backing up current yum.repos.d files..."
-mv /etc/yum.repos.d /etc/yum.repos.d.backup$RANDOM_NUMBER
+echo "Backing up current CentOS-Base.repo file ..."
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.orig
 
-echo "Creating new yum.repos.d directory..."
-mkdir /etc/yum.repos.d
+echo "Downloading and placing CentOS-Base.repo ..."
+curl -4 https://raw.githubusercontent.com/MrAriaNet/fix-centos7-repository/main/CentOS-Base.repo -o /etc/yum.repos.d/CentOS-Base.repo
 
-echo "Downloading and placing CentOS-Base.repo..."
-curl -4 https://raw.githubusercontent.com/MrAriaNet/fix-centos7-repository/refs/heads/main/CentOS-Base.repo -o /etc/yum.repos.d/CentOS-Base.repo
+if [[ -f /etc/yum.repos.d/epel.repo ]]; then
+    sed -i 's|#baseurl=http://download.example/pub/epel/7/|baseurl=https://mirrors.vcea.wsu.edu/centos-vault/epel-7/|' /etc/yum.repos.d/epel.repo
+    echo "epel.repo is fixed ..."
+fi
 
 yum clean all --verbose
 
